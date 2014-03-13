0c2ed325ecc7e0c7fd98425bda7d81fe4cdd65a8
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-03-13 15:24:24 -0400

    Fix patcon_gconf to not clone resources.

--- site-cookbooks/patcon/definitions/gconf.rb
+++ site-cookbooks/patcon/definitions/gconf.rb
@@ -1,5 +1,5 @@
 define :patcon_gconf, :attr_type => 'string' do
-  execute "set gconf setting" do
+  execute "set gconf setting: #{params[:name]}" do
     command "gconftool-2 --set #{params[:name]} --type #{params[:attr_type]} '#{params[:attr_val]}'"
     environment({"DBUS_SESSION_BUS_ADDRESS" => node['patcon']['dbus_session_bus_address']})
     user params[:user]
