9e87eb0bacc64ca2e6eecb7c032de1812b164d26
Patrick Connolly <patrick@myplanetdigital.com>
2014-03-24 01:05:45 -0700

    hub-cli version is now pluggable, so we can avoid buggy recent release.

--- site-cookbooks/patcon/recipes/hub.rb
+++ site-cookbooks/patcon/recipes/hub.rb
@@ -1,6 +1,8 @@
 require_recipe "patcon::base"
 
-gem_package "hub"
+gem_package "hub" do
+  version node['patcon']['hub_version']
+end
 
 user_home = node['etc']['passwd'][node['patcon']['user']]['dir']
 
